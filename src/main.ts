3bcb53603cc3ca7050b1433927ea5943a5b7c856
unknown <paulo.cayres@gmail.com>
2020-04-15 20:52:59 -0300

    troca de imperium pra premium

--- src/main.ts
+++ src/main.ts
@@ -31,6 +31,6 @@ async function bootstrap() {
       outputStyle: 'compressed',
     }),
   );
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
